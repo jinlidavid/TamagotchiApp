4597408c03109e6a76b48822dd846e91a3e799ee
David Jin Li <jin_li.d@etoncollege.org.uk>
2022-01-20 12:18:54 +0000

    Initial Commit

---
+++ TamagotchiApp/TamagotchiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiAppApp.swift
+//  TamagotchiApp
+//
+//  Created by Jin Li, David (MAG) on 20/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
