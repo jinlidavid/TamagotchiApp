7f4144db2dc4eee47520aa0ac7a1cbd2595f74d9
David Jin Li <jin_li.d@etoncollege.org.uk>
2022-01-25 11:13:36 +0000

    Progress with checking whether it is alive

--- TamagotchiApp/ContentView.swift
+++ TamagotchiApp/ContentView.swift
@@ -9,16 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var tamagotchi = Tamagotchi()
+    @State private var pooCounter = 0
+    @State private var timePassed = 0
+    let timer = Timer.publish(every:1, on: .main, in: .common).autoconnect()
     var body: some View {
-        Form{
-            VStack(alignment: .leading, spacing: 20) {
-                Text(tamagotchi.displayStats())
-                Button("Feed", action: {tamagotchi.eat()})
-                Button("Play", action: {tamagotchi.playGames()})
+        VStack {
+            Text("\(timePassed)")
+                .onReceive(timer) {_ in
+                    if !tamagotchi.isDead {
+                        self.timePassed += 1
+                        if self.timePassed % 15 == 0 {
+                            tamagotchi.toiletCheck()
+                        }
+                        if self.timePassed % 30 == 0 {
+                            tamagotchi.ageUp()
+                        }
+                        tamagotchi.checkHealth()
+                    }
+                }
+            
+            Form{
+                VStack(alignment: .leading, spacing: 20) {
+                    Text(tamagotchi.displayStats())
+                }
+                Section {
+                    Button("Feed (Snack)", action: {tamagotchi.eatSnack()})
+                    Button("Feed (Meal)", action: {tamagotchi.eatMeal()})
+                    Button("Play", action: {tamagotchi.playGames()})
+                    Button("Clean Up", action: {tamagotchi.cleanUp()})
+                }
+                .disabled(tamagotchi.isDead)
             }
         }
-        
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
--- TamagotchiApp/Tamagotchi.swift
+++ TamagotchiApp/Tamagotchi.swift
@@ -8,20 +8,41 @@
 import Foundation
 
 class Tamagotchi:ObservableObject {
+    @Published var isDead: Bool
     @Published var hunger: Int
     @Published var weight: Int
     @Published var happiness: Int
+    @Published var isIll: Bool
+    @Published var age: Int
+    //Toilet-related
+    @Published var needsToilet: Bool
+    @Published var cleanUpRequired: Bool
+    @Published var pooCounter: Int
     
     init() {
+        self.isDead = false
         self.hunger = 5
         self.weight = 4
         self.happiness = 3
+        self.isIll = false
+        self.needsToilet = false
+        self.pooCounter = 0
+        self.cleanUpRequired = false
+        self.age = 0
     }
     
-    func eat() {
+    func eatSnack() {
         if hunger > 0 {
             hunger -= 1
             weight += 2
+            happiness += 1
+        }
+    }
+    
+    func eatMeal() {
+        if hunger > 0 {
+            hunger -= 1
+            weight += 1
         }
     }
     
@@ -29,11 +50,45 @@ class Tamagotchi:ObservableObject {
         happiness += 1
     }
     
+    func toiletCheck() {
+        if needsToilet == true {
+            pooCounter += 1
+            cleanUpRequired = true
+        } else {
+            needsToilet = true
+        }
+    }
+    
+    func cleanUp() {
+        if pooCounter > 0{
+            pooCounter -= 1
+        }
+        if pooCounter == 0{
+            cleanUpRequired = false
+        }
+        needsToilet = false
+    }
+    
+    func ageUp() {
+        age += 1
+    }
+    
+    func checkHealth() {
+        let chanceOfDeath = Int.random(in: 1..<100)
+        if chanceOfDeath >= 98 {
+            isDead = true
+        }
+    }
+    
     func displayStats() -> String {
         return """
+            Alive: \(isDead ? "No" : "Yes")
+            Age: \(age) years
             Hunger: \(hunger)
             Weight: \(weight)lb
             Happiness: \(happiness)
+            Toilet Status: \(needsToilet ? "Needed" : "Not required")
+            Mess created: \(pooCounter)
             """
     }
 }
